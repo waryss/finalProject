0b2cd886d40883dbc719942671bd9a7d419fb24d
waryss <warysswiser@gmail.com>
2018-11-01 00:01:18 +0100

    get by criteria

--- src/main/java/project/dao/impl/TransactionDaoImpl.java
+++ src/main/java/project/dao/impl/TransactionDaoImpl.java
@@ -1,21 +1,18 @@
 package project.dao.impl;
 
-import java.time.LocalDate;
-
-import java.util.List;
-
-import javax.persistence.TypedQuery;
-
 import org.springframework.stereotype.Repository;
 import project.dao.TransactionDao;
 import project.model.Transaction;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public class TransactionDaoImpl extends GenericDaoImpl<Transaction, Integer> implements TransactionDao {
-	
-	public List<Transaction> getStatement(LocalDate dateFrom,LocalDate dateTo){
-    	String hql = "From account_transactions where date between "+dateFrom+"and"+dateTo;
-    	TypedQuery<Transaction> statementList = (TypedQuery<Transaction>) super.getEntityManager().createQuery(hql);
-    	return statementList.getResultList();
-	}
+
+    public List<Transaction> getStatement(LocalDate dateFrom, LocalDate dateTo) {
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder();
+        return getByCriteria(criteriaBuilder.between(getRoot(criteriaBuilder).get("date"), dateFrom, dateTo));
+    }
 }
